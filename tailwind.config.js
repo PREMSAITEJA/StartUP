9ee50d0198221c9fb34fea59c7fc8a7c4552a523
THULVA PRAM SAI TEJA <Premsaiteja58@gmail.com>
2025-05-13 10:21:41 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -34,9 +34,13 @@ module.exports = {
         'light-3': '#7878A3',
         'light-4': '#5C5C7B',
       },
-      screens: {
-        'xs': '480px',
-      
+     screens: {
+        xs: "450px", // Extra small screens
+        sm: "640px", // Small screens
+        md: "768px", // Medium screens
+        lg: "1024px", // Large screens
+        xl: "1280px", // Extra large screens
+        "2xl": "1536px", // 2x extra large screens
       },
       width: {
         '420': '420px',
